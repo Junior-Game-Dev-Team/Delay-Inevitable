a6729f1e7d9c4a880764283f7a91494703ab0cf5
Christer Bjoerk <productivity.christerbjoerk@gmail.com>
2023-12-13 15:22:00 +1000

    Loading screen
    Update scene loader to include a loading screen while loading the next level.

--- Assets/Horror Game/Scripts/Utilities/SceneLoadManager.cs
+++ Assets/Horror Game/Scripts/Utilities/SceneLoadManager.cs
@@ -2,11 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class SceneLoadManager : MonoBehaviour
 {
+    [SerializeField] private GameObject loadingCanvas;
+
+    [SerializeField] private Slider loadingBar;
+
+    private float progressValue = 0f;
+
     public void LoadSceneAsync(int sceneIndex) 
     {
-        SceneManager.LoadSceneAsync(sceneIndex);
+        loadingCanvas.SetActive(true);
+        StartCoroutine(LoadLeveAsync(sceneIndex));
+    }
+
+    IEnumerator LoadLeveAsync(int sceneIndex)
+    {
+
+        yield return new WaitForSeconds(2);
+        AsyncOperation loadOperation = SceneManager.LoadSceneAsync(sceneIndex);
+
+        while (!loadOperation.isDone && loadingBar.value != loadingBar.maxValue)
+        {
+            progressValue = Mathf.Clamp01(loadOperation.progress / 0.9f);
+            loadingBar.value = progressValue;   
+            yield return null;
+        }
     }
 }
