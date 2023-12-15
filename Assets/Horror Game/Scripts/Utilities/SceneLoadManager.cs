a982b511eb2a75ccfd9c6716d70cef7a6b8e710d
Christer Bjoerk <productivity.christerbjoerk@gmail.com>
2023-12-15 13:44:50 +1000

    Scene loader
    Added comments and minor code changes.

--- Assets/Horror Game/Scripts/Utilities/SceneLoadManager.cs
+++ Assets/Horror Game/Scripts/Utilities/SceneLoadManager.cs
@@ -6,27 +6,35 @@ using UnityEngine.UI;
 
 public class SceneLoadManager : MonoBehaviour
 {
-    [SerializeField] private GameObject loadingCanvas;
-
+    [Header("Config")]
+    [SerializeField] private GameObject loadingScreen;
     [SerializeField] private Slider loadingBar;
+    [SerializeField] private float loadingTimeDelay = 2.0f;
 
     private float progressValue = 0f;
 
     public void LoadSceneAsync(int sceneIndex) 
     {
-        loadingCanvas.SetActive(true);
-        StartCoroutine(LoadLeveAsync(sceneIndex));
+        // Enable loading screen
+        loadingScreen.SetActive(true);
+
+        StartCoroutine(LoadLevelAsync(sceneIndex));
     }
 
-    IEnumerator LoadLeveAsync(int sceneIndex)
+    IEnumerator LoadLevelAsync(int sceneIndex)
     {
+        // Delay the loading process
+        // Give time for players to see the loading screen
+        yield return new WaitForSeconds(loadingTimeDelay);
 
-        yield return new WaitForSeconds(2);
+        // Measure the loading of a new scene
         AsyncOperation loadOperation = SceneManager.LoadSceneAsync(sceneIndex);
 
         while (!loadOperation.isDone && loadingBar.value != loadingBar.maxValue)
         {
+            // Convert the progress to a value between 0 and 1
             progressValue = Mathf.Clamp01(loadOperation.progress / 0.9f);
+            // Apply progress
             loadingBar.value = progressValue;   
             yield return null;
         }
